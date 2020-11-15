bd7ffd25365b51e284148d01ce1b4698fa8d8a45
Julien Blondeau <julienblondeau@live.fr>
2020-11-15 18:00:34 +0100

    header footer

--- src/Blog/BlogModule.php
+++ src/Blog/BlogModule.php
@@ -1,25 +1,30 @@
 <?php
 namespace App\Blog;
 
+use Framework\Renderer;
 use Framework\Router;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class BlogModule {
 
+  private $renderer;
+
 
   public function __construct(Router $router)
   {
+    $this->renderer = new Renderer();
+    $this->renderer->addPath('blog', __DIR__ . '/views');
     $router->get('/blog', [$this, 'index'], 'blog.index');
     $router->get('/blog/{slug:[a-z\-]+}', [$this, 'show'], 'blog.show');
   }
 
   public function index(Request $request): string
   {
-    return '<h1>Bienvenue sur le blog</h1>';
+    return $this->renderer->render('@blog/index');
   }
 
   public function show(Request $request): string
   {
-    return '<h1>Bienvenue sur l\'article ' . $request->getAttribute('slug') . '</h1>';
+    return $this->renderer->render('@blog/show');
   }
 }
---
+++ src/Blog/views/index.php
@@ -0,0 +1,13 @@
+<?php ?>
+
+<h1>Bienvenue sur le blog</h1>
+
+<ul>
+  <li>a</li>
+  <li>a</li>
+  <li>a</li>
+  <li>a</li>
+</ul>
+
+<?php ?>
+
